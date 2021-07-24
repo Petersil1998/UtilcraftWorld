488eabfdbf236fa5c1d1583ee37bcf3b78d07793
Petersil1998 <silbernagl.peter@rolmail.net>
2021-07-24 23:42:15 +0200

    Players spawn in Afterlife dimension when dying

--- src/main/java/net/petersil98/utilcraft_world/event/EventHandler.java
+++ src/main/java/net/petersil98/utilcraft_world/event/EventHandler.java
@@ -1,9 +1,26 @@
 package net.petersil98.utilcraft_world.event;
 
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.petersil98.utilcraft_world.UtilcraftWorld;
+import net.petersil98.utilcraft_world.utils.PlayerUtils;
 
 @Mod.EventBusSubscriber(modid = UtilcraftWorld.MOD_ID)
 public class EventHandler {
 
+    @SubscribeEvent
+    public static void onPlayerDeath(PlayerEvent.PlayerRespawnEvent event) {
+        if(event.getPlayer() instanceof ServerPlayerEntity) {
+            ServerPlayerEntity player = (ServerPlayerEntity) event.getPlayer();
+            BlockPos respawnPoint = player.func_241140_K_();
+            if(respawnPoint == null) {
+                respawnPoint = player.world.getServer().getWorld(World.OVERWORLD).getSpawnPoint();
+            }
+            PlayerUtils.teleportToWorld(player, UtilcraftWorld.AFTERLIFE_WORLD, respawnPoint, true);
+        }
+    }
 }
